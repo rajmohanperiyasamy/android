5e6b4d1cb2bd7fcf99726f43ed45ef6e823f70e2
rajmohanperiyasamy <rajmohanjr@gmail.com>
2015-07-29 22:27:52 +0530

    Update settings.py

--- android/settings.py
+++ android/settings.py
@@ -36,7 +36,7 @@ EMAIL_USE_TLS = True
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'rajmohanjr@gmail.com'
-EMAIL_HOST_PASSWORD = 'doublespring1'
+EMAIL_HOST_PASSWORD = 'PPPPPPPPPPPPPPPP'
 DEFAULT_FROM_EMAIL = ''
 SERVER_EMAIL = '<address>'
 
